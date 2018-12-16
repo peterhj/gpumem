2f4fd83b9ec3d6a1b3c1f702962e5ab2a7939aa1
Peter Jin <peterhaijin@gmail.com>
2018-12-16 15:58:00 -0800

    Add memory region traits.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 extern crate cudart;
 //extern crate memrepr;
 
-use cudart::{CudaStream, cuda_memset_async};
+use crate::ctx::{GpuCtxGuard};
+
+use cudart::{CudaStream, cuda_alloc_device, cuda_memset_async};
 //use memrepr::{ZeroBits};
 
 use std::mem::{size_of};
@@ -11,16 +13,24 @@ use std::slice::{from_raw_parts, from_raw_parts_mut};
 
 pub mod ctx;
 
+pub trait GpuRegion<T: Copy + 'static> {
+  unsafe fn as_devptr(&self) -> *const T;
+}
+
+pub trait GpuRegionMut<T: Copy + 'static> {
+  unsafe fn as_devptr_mut(&self) -> *mut T;
+}
+
 pub trait GpuDelay {
   type Target: Copy + 'static;
 }
 
 pub trait GpuDelayed<V: GpuDelay> {
-  unsafe fn dptr(&self) -> *const V::Target;
+  fn dptr(&self) -> *const V::Target;
 }
 
-pub trait GpuDelayedMut<V: GpuDelay>: GpuDelayed<V> {
-  unsafe fn dptr_mut(&self) -> *mut V::Target;
+pub trait GpuDelayedMut<V: GpuDelay> {
+  fn dptr_mut(&self) -> *mut V::Target;
 }
 
 pub struct GpuUnsafePinnedMem<T: Copy + 'static> {
@@ -31,6 +41,13 @@ pub struct GpuUnsafePinnedMem<T: Copy + 'static> {
 
 impl<T: Copy + 'static> GpuDelay for GpuUnsafePinnedMem<T> {
   type Target = T;
+}
+
+impl<T: Copy + 'static> Drop for GpuUnsafePinnedMem<T> {
+  fn drop(&mut self) {
+    // TODO
+    unimplemented!();
+  }
 }
 
 impl<T: Copy + 'static> GpuUnsafePinnedMem<T> {
@@ -59,6 +76,42 @@ impl<T: Copy + 'static> GpuDelay for GpuUnsafeMem<T> {
   type Target = T;
 }
 
+impl<T: Copy + 'static> GpuRegion<T> for GpuUnsafeMem<T> {
+  unsafe fn as_devptr(&self) -> *const T {
+    self.dptr
+  }
+}
+
+impl<T: Copy + 'static> GpuRegionMut<T> for GpuUnsafeMem<T> {
+  unsafe fn as_devptr_mut(&self) -> *mut T {
+    self.dptr
+  }
+}
+
+impl<T: Copy + 'static> Drop for GpuUnsafeMem<T> {
+  fn drop(&mut self) {
+    // TODO: do nothing for now.
+    //unimplemented!();
+  }
+}
+
+impl<T: Copy + 'static> GpuUnsafeMem<T> {
+  pub unsafe fn alloc(len: usize, dev: i32) -> GpuUnsafeMem<T> {
+    let _ctx = GpuCtxGuard::new(dev);
+    let bysz = len * size_of::<T>();
+    let dptr = match cuda_alloc_device(bysz) {
+      Err(e) => panic!("cudaMalloc failed: {:?} ({})", e, e.get_string()),
+      Ok(dptr) => dptr,
+    };
+    GpuUnsafeMem{
+      dptr,
+      len,
+      bysz,
+      dev,
+    }
+  }
+}
+
 /*impl<T: ZeroBits + Copy + 'static> GpuUnsafeMem<T> {
   pub unsafe fn set_zeros(&self, stream: &mut CudaStream) {
     let res = cuda_memset_async(
@@ -73,25 +126,3 @@ impl<T: Copy + 'static> GpuDelay for GpuUnsafeMem<T> {
     }
   }
 }*/
-
-impl<T: Copy + 'static> Drop for GpuUnsafeMem<T> {
-  fn drop(&mut self) {
-    // TODO
-    unimplemented!();
-  }
-}
-
-impl<T: Copy + 'static> GpuUnsafeMem<T> {
-  pub unsafe fn alloc(len: usize, dev: i32) -> GpuUnsafeMem<T> {
-    // TODO
-    unimplemented!();
-  }
-
-  pub unsafe fn as_devptr(&self) -> *const T {
-    self.dptr
-  }
-
-  pub unsafe fn as_devptr_mut(&self) -> *mut T {
-    self.dptr
-  }
-}
